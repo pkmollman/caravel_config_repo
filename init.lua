fd496f0e1bda47c3a6f94e5060b63e77af5c75be
Patrick Mollman <pkmollman@gmail.com>
2026-03-01 01:31:08 -0500

    change

--- init.lua
+++ init.lua
@@ -4,32 +4,9 @@ local dir = File {
     path = "/test-from-caravel2",
 }
 
-function tprint(tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    local formatting = string.rep("  ", indent) .. k .. ": "
-
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    elseif type(v) == "boolean" then
-      print(formatting .. tostring(v))
-    elseif type(v) == "userdata" then
-      -- Handle userdata by converting to string
-      print(formatting .. tostring(v))  -- Use tostring to safely print userdata
-    else
-      print(formatting .. tostring(v))  -- Convert all other types to string
-    end
-  end
-end
-
-tprint(dir)
-
-print(dir.gid)
-
 for i = 1, 10000 do
     File {
-        state = "Present",
+        state = "Absent",
         file_type = "File",
         path = dir.path .. "/file" .. i .. ".txt",
         content = "hi there " .. i .. "\n",
