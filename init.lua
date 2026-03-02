b77256915787232896872c228c768572d5c47221
Patrick Mollman <pkmollman@gmail.com>
2026-03-01 20:39:58 -0500

    stuff

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ local dir = File {
 for i = 1, 10000 do
     File {
         state = "Absent",
-        file_type = "File",
+        -- file_type = "File",
         path = dir.path .. "/file" .. i .. ".txt",
         content = "hi there " .. i .. "\n",
     }
@@ -38,3 +38,15 @@ print("root level thing.name: " .. root_level_thing.name)
 local nested_test_level_thing = require("nested_package.test")
 
 print("nested test level thing.name: " .. nested_test_level_thing.name)
+
+-- local file_owner_uid = caravel.core.file { path = "/opt/somefile" }.uid
+-- local children = caravel.core.find { path = "/var/run/somedirectory" }
+-- local file_paths = {}
+-- for i, child in ipairs(children) do
+--     if child.type == "directory" then
+--         table.insert(file_paths, child.path)
+--     end
+-- end
+-- for i, fpath in ipairs(file_paths) do
+--     caravel.core.file { path = fpath, uid = file_owner_uid }
+-- end
